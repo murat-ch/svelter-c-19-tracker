9cc7c5eee4a70ade2224d75401bc00bbe2a9f44e
murat-ch <muratik.ch@gmail.com>
2020-10-21 07:34:24 -0400

    got about page + titles for all pages

---
+++ src/routes/about.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <title>About Covid-19 Tracker</title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+        <h1>About</h1>
+        <p>
+            Tha data for this site came from <a href="https://covidtracking.com">Covid Tracking Project</a>. You can learn more about by going to their site.
+        </p>
+    </div>
+</div>
